e55f1632f8743ad9f44087407c6fc9a0242aabdd
Sunny Chan <schan6803@gmail.com>
2024-11-22 13:25:54 -0500

    establishing page routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import './styles/index.css'
 import App from './App.jsx'
+import Database from './components/Database.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/montserrat-app/',
+    element: <App />,
+    children: [
+      {
+        path: 'database',
+        element: <Database/>
+      }
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
