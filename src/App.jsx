586ad323681e357fc5d0311485521d3ca7ec622d
schan523 <schan6803@gmail.com>
2024-11-22 18:44:03 -0500

    routing changes for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
 import './styles/App.css'
-import Home from './components/Home.jsx'
 import Navbar from './components/Navbar.jsx'
-import { Outlet } from 'react-router'
+import { Routes, Route } from 'react-router-dom'
+import Home from './components/Home.jsx'
+import Database from './components/Database.jsx'
+import Donation from './components/Donation.jsx'
+import Updates from './components/Updates.jsx'
 
 export default function App() {
   const [count, setCount] = useState(0)
@@ -14,8 +17,12 @@ export default function App() {
   return (
     <>
       <Navbar />
-      <Home />
-      <Outlet />
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/database" element={<Database/>}/>
+        <Route path="/donation" element={<Donation/>}/>
+        <Route path="/updates" element={<Updates/>}/>
+      </Routes>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './styles/index.css'
 import App from './App.jsx'
-import Database from './components/Database.jsx'
-import Home from './components/Home.jsx'
-import Updates from './components/Updates.jsx'
-import Donation from './components/Donation.jsx'
-
-const router = createBrowserRouter([
-  {
-    path: '/montserrat-app/',
-    element: <App />,
-    children: [
-      {
-        path: 'database',
-        element: <Database/>
-      },
-      {
-        index: true,
-        element: <Home/>
-      },
-      {
-        path: 'updates',
-        element: <Updates/>
-      },
-      {
-        path: 'donation',
-        element: <Donation/>
-      }
-    ]
-  }
-]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <HashRouter>
+      <App/>
+    </HashRouter>
   </StrictMode>,
 )
