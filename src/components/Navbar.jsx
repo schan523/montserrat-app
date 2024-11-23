ba9cb934cd2d76f3e6fb1c9a8df7f19c550946cf
schan523 <schan6803@gmail.com>
2024-11-22 19:23:53 -0500

    fixing navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../App.jsx'
-import './../styles/Navbar.module.css'
+import css from './../styles/Navbar.module.css'
 
 export default function Navbar() {
     return (
-    <nav className="App">
-        <ul>
-            <li><Link to="">Home</Link></li>
-            <li><Link to="database">Database</Link></li>
-            <li><Link to="updates">Updates</Link></li>
-            <li><Link to="donation">Donation</Link></li>
+    <nav className={css.nav}>
+        <ul className={css.ul}>
+            <li className={css.navItem}><Link to="">Home</Link></li>
+            <li className={css.navItem}><Link to="database">Database</Link></li>
+            <li className={css.navItem}><Link to="updates">Updates</Link></li>
+            <li className={css.navItem}><Link to="donation">Donation</Link></li>
         </ul>
     </nav>
 );
