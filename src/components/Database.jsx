c44a03348f2a1279e2d3e2874187df2f2b5866e9
schan523 <schan6803@gmail.com>
2024-11-22 21:00:42 -0500

    database page work

--- src/components/Database.jsx
+++ src/components/Database.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import css from '../styles/Database.module.css'
 
 export default function Database() {
+    // let data;
+    
+    // fetch("profiles.json")
+    // .then((response) => {
+    //     return response.json();
+    // })
+    // .then(d => {
+    //     console.log(d);
+    // })
+
+    // console.log(data);
+    
     return (
-    <div>Database</div>
+    <div className={css.container}>
+        <h2>Database</h2>
+        <button>Upload JSON</button>
+        <br/>
+        <input type="search" placeholder="Search"/>
+        <div className={css.databaseContainer}>
+
+        </div>
+    </div>
+    
 );
 }
